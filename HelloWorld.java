81ff2e0408c902b86082a9e53d404688883dbc30
Kamerin V <kamerin.vesajd@pcc.edu>
2021-01-28 21:16:23 -0800

    Added a print date

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,16 @@
 package edu.pcc.cis.helloworld;
+import java.util.Date;
+
+/**
+ * Made by Kamerin Vesajd @ 1/28/2021
+ */
 
 public class HelloWorld {
 
     public static void main(String[] args)
     {
         System.out.println("Hello World");
+        System.out.println(new Date());
     }
-
 
 }
